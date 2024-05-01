d7bf1c061cf97f592e18faab7b279338c7da6f83
isslem ziedi <123024361+isslemziedi@users.noreply.github.com>
2024-05-01 12:27:51 +0200

    Add files via upload

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Form;
+
+use App\Entity\Article;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType; // Importez TextType pour définir les champs texte
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, ['label' => 'Nom de l\'article' ])
+            ->add('prix', TextType::class, ['label' => 'Prix de l\'article']);
+    }
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(['data_class' => Article::class,]);
+    }
+}
